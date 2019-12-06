1854216563f427443412b2b013890dc286025abb
DeboraRodrigues1 <57760416+DeboraRodrigues1@users.noreply.github.com>
2019-12-06 15:02:10 -0300

    Atualização

---
+++ exemplo.js
@@ -0,0 +1,5 @@
+//carrego a biblioteca que vimos e atribui á variavell libINput
+var libInput = require('readline-sync');
+
+var algumaCoisa = libInput.question("Digite algo...");
+console.log("Você digitou " + algumaCoisa);
